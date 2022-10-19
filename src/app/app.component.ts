7a3fc8364b007bc057d26acd2911bbe0af6d391e
Tito <50543732+DevWebJr@users.noreply.github.com>
2022-10-19 12:43:55 +0200

    update functions about displaying the selected pokemon on app

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {POKEMONS} from "./mock-pokemons";
+import {Pokemon} from "./pokemon";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+  })
+
+export class AppComponent implements OnInit{
+  pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon | undefined;
+
+  ngOnInit() {
+    console.table(this.pokemonList);
+  }
+
+  selectOnePokemon(pokemonId: string) {
+    const pokemon: Pokemon | undefined = this.pokemonList.find(pokemon => pokemon.id == + pokemonId)
+
+    if(pokemon) {
+      console.log(`Vous avez sélectionné le Pokémon ${pokemon?.name}`);
+      this.pokemonSelected = pokemon;
+    }
+    else {
+      console.log("Ce nombre ne correspond à aucun Pokémon.");
+      this.pokemonSelected = pokemon;
+    }
+  }
+
+}
